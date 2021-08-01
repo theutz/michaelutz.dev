dd4c4219f20650f4eed434b9bd2156f240f00814
Michael Utz <michael@theutz.com>
2021-08-02 01:34:28 +0300

    refactor(pages/home): extract `<Footer />`

---
+++ components/Footer.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import { Footer } from "./Footer";
+
+describe("<Footer />", () => {
+  it("renders", () => {
+    // Arrange
+
+    // Act
+    const { asFragment } = render(<Footer />);
+
+    // Assert
+    expect(asFragment()).toMatchSnapshot();
+  });
+});

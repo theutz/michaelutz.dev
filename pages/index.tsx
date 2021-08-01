82f757681f4557634c2e4b99df72068702827a6f
Michael Utz <michael@theutz.com>
2021-08-01 11:50:24 +0300

    feat: add favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,5 +2,17 @@ import Head from "next/head";
 import Image from "next/image";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Head>
+        <link
+          rel="icon"
+          type="image/svg+xml"
+          href="/assets/images/favicon.svg"
+        />
+        <link rel="icon" type="image/png" href="/assets/images/favicon.png" />
+      </Head>
+      Hello
+    </div>
+  );
 }

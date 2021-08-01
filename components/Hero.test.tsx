e70801694f64aaf0960ba32542d221061822ddad
Michael Utz <michael@theutz.com>
2021-08-02 01:15:57 +0300

    refactor(pages/home): extract `<Hero />`

---
+++ components/Hero.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import { Hero } from "./Hero";
+
+describe("<Hero />", () => {
+  it("renders", () => {
+    // Arrange
+
+    // Act
+    const { asFragment } = render(<Hero />);
+
+    // Assert
+    expect(asFragment()).toMatchSnapshot();
+  });
+});

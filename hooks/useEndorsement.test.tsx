4be3c0cdf60f384e57d2ec78e49b7a9fdd2a024c
Michael Utz <michael@theutz.com>
2021-08-03 00:08:28 +0300

    feat(hooks): add images to `useEndorsement()`

--- hooks/useEndorsement.test.tsx
+++ hooks/useEndorsement.test.tsx
@@ -1,14 +1,63 @@
 import { renderHook, act } from "@testing-library/react-hooks";
-import { useEndorsement, endorsements } from "hooks/useEndorsement";
+import { useEndorsement, Endorsement } from "hooks/useEndorsement";
+
+const data: readonly Endorsement[] = [
+  {
+    name: "name 1",
+    title: "title 1",
+    content: "content 1",
+    image: { src: "/path-1", width: 100, height: 100 },
+    company: "company 1",
+  },
+  {
+    name: "name 2",
+    title: "title 2",
+    content: "content 2",
+    image: { src: "/path-2", width: 100, height: 100 },
+    company: "company 2",
+  },
+];
 
 describe(`useEndorsement`, () => {
-  it(`fails`, () => {
-    const { result } = renderHook(() => useEndorsement());
+  it(`throws when data is empty`, () => {
+    expect(() => useEndorsement([])).toThrow(`Endorsements cannot be empty.`);
+  });
 
+  it(`returns a default item`, () => {
+    // Arrange
+    // Act
+    const { result } = renderHook(() => useEndorsement(data));
+
+    // Assert
+    expect(result.current.endorsement).toMatchObject(data[0]);
+  });
+
+  it("returns a new item after incrementing", () => {
+    // Arrange
+    const { result } = renderHook(() => useEndorsement(data));
+
+    // Act
     act(() => {
       result.current.increment();
     });
 
-    expect(result.current.endorsement).toMatchObject(endorsements[1]);
+    // Assert
+    expect(result.current.endorsement).toMatchObject(data[1]);
+  });
+
+  it("resets to initial item when list is exhausted", () => {
+    // Arrange
+    const { result } = renderHook(() => useEndorsement(data));
+
+    // Act
+    act(() => {
+      result.current.increment();
+    });
+    act(() => {
+      result.current.increment();
+    });
+
+    // Assert
+    expect(result.current.endorsement).toMatchObject(data[0]);
   });
 });
--- hooks/useEndorsement.tsx
+++ hooks/useEndorsement.tsx
@@ -1,10 +1,14 @@
+import { ImageProps } from "next/image";
 import { useCallback, useState } from "react";
+import benWright from "public/assets/images/ben-wright.jpg";
+import sydneyBolinger from "public/assets/images/sydney-bolinger.jpg";
 
-type Endorsement = {
+export type Endorsement = {
   name: string;
   title: string;
   company: string;
   content: string;
+  image: Exclude<ImageProps["src"], string>;
 };
 
 export const endorsements: readonly Endorsement[] = [
@@ -12,14 +16,16 @@ export const endorsements: readonly Endorsement[] = [
     name: "Benjamin Wright",
     title: "Director of Ecommerce",
     company: "Space Race",
+    image: benWright,
     content: `
 I worked alongside Michael on the Web Development Team at FRN/UHS. Mike is super friendly and top of the line at his craft. His work in DevOps and JavaScript elevated our team's workflow to the next level, making everyone on our team better at what they do. Mike was our "go-to" guy whenever we had complex development issues. He always communicated clearly and timely while consistently delivering excellent results. It was a privilege to learn from and work with Mike!
 `,
   },
   {
-    name: "Sydney Ratzlaff",
+    name: "Sydney Bolinger",
     title: "Director of Marketing",
     company: "Bible Project",
+    image: sydneyBolinger,
     content: `
 I had the pleasure of working alongside Michael at Emergency Reporting. We worked on several projects and campaigns together, and he truly helped me grow in my own abilities.
  
@@ -32,7 +38,9 @@ I would highly recommend Michael for future roles and responsibilities, as well 
   },
 ];
 
-export function useEndorsement() {
+export function useEndorsement(data: readonly Endorsement[] = endorsements) {
+  if (data.length === 0) throw new Error(`Endorsements cannot be empty.`);
+
   const defaultIndex = 0;
   const [index, setIndex] = useState(defaultIndex);
 
@@ -41,14 +49,16 @@ export function useEndorsement() {
   }, [setIndex]);
 
   const increment = useCallback(() => {
-    if (index === endorsements.length) {
-      return reset();
+    if (index === data.length - 1) {
+      reset();
+      return;
     }
 
-    return setIndex((prev) => prev + 1);
-  }, [index, setIndex, reset]);
+    setIndex((prev) => prev + 1);
+    return;
+  }, [index, setIndex, reset, data]);
 
-  const endorsement = endorsements[index];
+  const endorsement = data[index];
 
   return { endorsement, increment, reset };
 }

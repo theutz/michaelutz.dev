17bf779edb2076d1a21d5c6fba0774a5517c0ed1
Michael Utz <michael@theutz.com>
2021-08-02 11:07:46 +0300

    refactor(layouts): extract `<DefaultLayout />`

---
+++ components/layouts/Default.tsx
@@ -0,0 +1,21 @@
+import { ReactNode } from "react";
+import Head from "../Head";
+import Header from "../Header";
+import Footer from "../Footer";
+
+export type Props = {
+  children: ReactNode;
+};
+
+export function Default({ children }: Props) {
+  return (
+    <div className="bg-white">
+      <Head />
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </div>
+  );
+}
+
+export default Default;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,4 @@
 /* eslint-disable @next/next/no-img-element */
-import Header from "components/Header";
-import Footer from "components/Footer";
-import Head from "components/Head";
 import Hero from "components/pages/home/Hero";
 import LogoCloud from "components/pages/home/LogoCloud";
 import AlternatingFeatureSections from "components/pages/home/AlternatingFeatures";
@@ -11,19 +8,14 @@ import CTA from "components/pages/home/CTA";
 
 export function Index() {
   return (
-    <div className="bg-white">
-      <Head />
-      <Header />
-      <main>
-        <Hero />
-        <LogoCloud />
-        <AlternatingFeatureSections />
-        <GradientFeatures />
-        <Stats />
-        <CTA />
-      </main>
-      <Footer />
-    </div>
+    <>
+      <Hero />
+      <LogoCloud />
+      <AlternatingFeatureSections />
+      <GradientFeatures />
+      <Stats />
+      <CTA />
+    </>
   );
 }
 

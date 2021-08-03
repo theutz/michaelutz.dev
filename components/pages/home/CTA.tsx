b4ff82d403be1ed540d0df791992d70a4bbae1a7
Michael Utz <michael@theutz.com>
2021-08-03 23:38:30 +0300

    feat(pages/home): write call to action section

--- components/pages/home/CTA.tsx
+++ components/pages/home/CTA.tsx
@@ -5,7 +5,7 @@ export function CTA() {
         <h2 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
           <span className="block">Ready to get started?</span>
           <span className="block text-transparent bg-gradient-to-r from-purple-600 to-indigo-600 bg-clip-text">
-            Get in touch or create an account.
+            Get in touch!
           </span>
         </h2>
         <div className="mt-6 space-y-4 sm:space-y-0 sm:flex sm:space-x-5">
@@ -13,13 +13,7 @@ export function CTA() {
             href="#"
             className="flex items-center justify-center px-4 py-3 text-base font-medium text-white border border-transparent bg-gradient-to-r from-purple-600 to-indigo-600 bg-origin-border rounded-md shadow-sm hover:from-purple-700 hover:to-indigo-700"
           >
-            Learn more
-          </a>
-          <a
-            href="#"
-            className="flex items-center justify-center px-4 py-3 text-base font-medium text-indigo-800 border border-transparent rounded-md shadow-sm bg-indigo-50 hover:bg-indigo-100"
-          >
-            Get started
+            Contact Me
           </a>
         </div>
       </div>

dd9df547b40236301d7068fc3b1bf476cbdd035d
Michael Utz <michael@theutz.com>
2021-08-04 12:46:48 +0300

    feat(pages/api/subscribe): add initial api route implementation
    Using environment variables that will be set later

---
+++ pages/api/subscribe.tsx
@@ -0,0 +1,25 @@
+import mailchimp from "@mailchimp/mailchimp_marketing"
+
+mailchimp.setConfig({
+  apiKey: process.env.MAILCHIMP_API_KEY,
+  server: process.env.MAILCHIMP_API_SERVER,
+})
+
+export default async function subscribe(req, res) {
+  const { email } = res.body
+
+  if (!email) {
+    return res.status(400).json({ error: "Email is required" })
+  }
+
+  try {
+    await mailchimp.lists.adListMember(process.env.MAILCHIMP_AUDIENCE_ID, {
+      email_address: email,
+      status: "subscribed",
+    })
+
+    return res.status(201).json({ error: "" })
+  } catch (error) {
+    return res.status(500).json({ error: error.message || error.toString() })
+  }
+}

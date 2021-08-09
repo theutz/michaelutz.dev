142a6a4358a7bb3bfcf2607e0faf72c188c0b797
Michael Utz <michael@theutz.com>
2021-08-10 01:49:32 +0300

    refactor(header,e2e): wrap in describe block

--- cypress/integration/header.spec.ts
+++ cypress/integration/header.spec.ts
@@ -1,74 +1,76 @@
-before(() => {
-  cy.visit("/")
-})
+describe(`Header`, () => {
+  before(() => {
+    cy.visit("/")
+  })
 
-beforeEach(() => {
-  cy.get("[data-cy=header-nav]").as("header")
-})
-
-describe(`the logo`, () => {
   beforeEach(() => {
-    cy.get("@header").find("a[href='/']").as("link")
+    cy.get("[data-cy=header-nav]").as("header")
   })
 
-  it(`exists`, () => {
-    cy.get("@link").should("exist")
-  })
-
-  it(`has sr text`, () => {
-    cy.get("@header").find(".sr-only:contains(Michael Utz)").should("exist")
-  })
-
-  context(`when clicking`, () => {
+  describe(`the logo`, () => {
     beforeEach(() => {
-      cy.get("@link").click()
+      cy.get("@header").find("a[href='/']").as("link")
     })
 
-    it(`takes you to the home page`, () => {
-      cy.url().should("eq", Cypress.config().baseUrl + "/")
+    it(`exists`, () => {
+      cy.get("@link").should("exist")
     })
-  })
-})
 
-describe(`the links`, () => {
-  const hamburgerSelector = "[data-cy=hamburger-menu-button]"
-
-  const links = ["Pricing", "Partners", "Company"]
-  beforeEach(() => {
-    cy.get("[data-cy=hamburger-menu-button]").as("hamburger")
-  })
-
-  context(`on mobile`, { viewportHeight: 844, viewportWidth: 390 }, () => {
-    it(`is visible`, () => {
-      cy.get("@hamburger").should("be.visible")
+    it(`has sr text`, () => {
+      cy.get("@header").find(".sr-only:contains(Michael Utz)").should("exist")
     })
 
     context(`when clicking`, () => {
-      before(() => {
-        cy.get(hamburgerSelector).click()
-      })
-
       beforeEach(() => {
-        cy.get("[data-cy=hamburger-popover]").as("menu")
+        cy.get("@link").click()
       })
 
-      it(`shows the popover menu`, () => {
-        cy.get("@menu").should("be.visible")
+      it(`takes you to the home page`, () => {
+        cy.url().should("eq", Cypress.config().baseUrl + "/")
+      })
+    })
+  })
+
+  describe(`the links`, () => {
+    const hamburgerSelector = "[data-cy=hamburger-menu-button]"
+
+    const links = ["Pricing", "Partners", "Company"]
+    beforeEach(() => {
+      cy.get("[data-cy=hamburger-menu-button]").as("hamburger")
+    })
+
+    context(`on mobile`, { viewportHeight: 844, viewportWidth: 390 }, () => {
+      it(`is visible`, () => {
+        cy.get("@hamburger").should("be.visible")
       })
 
-      describe(`other links`, () => {
-        links.forEach((link) => {
-          it(`has the ${link} link`, () => {
-            cy.get("@menu").find(`a:contains(${link})`).should("be.visible")
+      context(`when clicking`, () => {
+        before(() => {
+          cy.get(hamburgerSelector).click()
+        })
+
+        beforeEach(() => {
+          cy.get("[data-cy=hamburger-popover]").as("menu")
+        })
+
+        it(`shows the popover menu`, () => {
+          cy.get("@menu").should("be.visible")
+        })
+
+        describe(`other links`, () => {
+          links.forEach((link) => {
+            it(`has the ${link} link`, () => {
+              cy.get("@menu").find(`a:contains(${link})`).should("be.visible")
+            })
           })
         })
       })
     })
-  })
 
-  context(`on larger screens`, () => {
-    it(`is not visible`, () => {
-      cy.get("@hamburger").should("not.be.visible")
+    context(`on larger screens`, () => {
+      it(`is not visible`, () => {
+        cy.get("@hamburger").should("not.be.visible")
+      })
     })
   })
 })

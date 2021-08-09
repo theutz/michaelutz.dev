ca3f3081b004286cfd957b8f52b8163f560152dd
Michael Utz <michael@theutz.com>
2021-08-09 16:26:08 +0300

    feat(header): fix sr-only company name

--- components/Header.tsx
+++ components/Header.tsx
@@ -50,7 +50,7 @@ export function Header() {
             <div className="flex items-center justify-between px-4 py-6 mx-auto max-w-7xl sm:px-6 md:justify-start md:space-x-10 lg:px-8">
               <div className="flex justify-start lg:w-0 lg:flex-1">
                 <a href="#">
-                  <span className="sr-only">Workflow</span>
+                  <span className="sr-only">Michael Utz</span>
                   <ChipIcon className="w-auto h-8 text-purple-700 stroke-current sm:h-10" />
                 </a>
               </div>

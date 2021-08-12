1637824dc628627177d0cd8409d89e12a453f04b
Michael Utz <michael@theutz.com>
2021-08-12 23:52:13 +0300

    chore(plop): setup templates for components

--- plopfile.js
+++ plopfile.js
@@ -24,8 +24,50 @@ module.exports = function (plop) {
       {
         type: "add",
         path: "cypress/integration/pages/{{kebabCase name}}.spec.ts",
+||||||| parent of 05f627f (chore(plop): setup templates for components)
+        path: "cypress/integration/pages/{{kebabCase name}}.ts",
         templateFile: `${templateBase}/page/e2e.hbs`,
       },
     ],
+  })
+
+  plop.setGenerator("component", {
+    description: "a component for use within a page",
+    prompts: [
+      {
+        type: "input",
+        name: "name",
+        message: "component name please",
+      },
+      {
+        type: "confirm",
+        name: "e2e",
+        message: "include e2e file?",
+      },
+    ],
+    actions: function (data) {
+      const actions = [
+        {
+          type: "add",
+          path: "components/{{pascalCase name}}.tsx",
+          templateFile: `${templateBase}/component/component.hbs`,
+        },
+        {
+          type: "add",
+          path: "components/{{pascalCase name}}.test.tsx",
+          templateFile: `${templateBase}/component/test.hbs`,
+        },
+      ]
+
+      if (data.e2e) {
+        actions.push({
+          type: "add",
+          path: "cypress/integration/components/{{pascalCase name}}.spec.ts",
+          templateFile: `${templateBase}/component/e2e.hbs`,
+        })
+      }
+
+      return actions
+    },
   })
 }
